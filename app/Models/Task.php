334072e7601959f593155d310ab5040740f0571d
ian_solano <iankarlhossolanogarcia@gmail.com>
2026-05-25 16:01:18 -0500

    migraciones y modelos con relaciones

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
-{
+{ 
+    protected $fillable = ['title', 'description', 'status', 'due_date', 'project_id'];
     public function project() {
          return $this->belongsTo(Project::class);
     }
